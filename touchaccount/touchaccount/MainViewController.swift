4143934d4327c002e2496922e1e1f90495f52ff7
jeff <xorgus1234@gmail.com>
2020-11-25 16:16:50 +0900

    project init

---
+++ touchaccount/touchaccount/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  touchaccount
+//
+//  Created by TakHyun Jung on 2020/11/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
