7f03ff274bf5dd54dc1a13ca533c861661930fcf
jeff <xorgus1234@gmail.com>
2020-12-27 03:46:57 +0900

    URL Scheme 수정

--- touchAcount/touchAcount/Bank.swift
+++ touchAcount/touchAcount/Bank.swift
@@ -33,6 +33,7 @@ enum Bank: String, CaseIterable {
 
 
 enum BankApp: String, CaseIterable {
+    case kakaopay = "카카오페이"
     case kakaobank = "카카오뱅크"
     case supertoss = "토스"
     case kbBank = "KB스타뱅킹"
@@ -42,9 +43,9 @@ enum BankApp: String, CaseIterable {
     case NHCOK = "NH콕뱅크"
     case NHAllOneBank = "NH올원뱅크"
     case ionebank = "IBK기업은행"
-    case sbankmoasign = "신한 쏠"
+    case sbankmoasign = "신한 쏠(SOL)"
     case smartkdb = "스마트KDB"
-    case NewSmartPib = "우리WON뱅킹"
+    case NewSmartPib = "우리 WON 뱅킹"
     case SmartBank2WIB = "위비뱅크"
     case KTollehCitiMobile = "시티모바일"
     case hanapush = "하나원큐"
@@ -59,9 +60,24 @@ enum BankApp: String, CaseIterable {
     case mgsangsangbank = "MG상상뱅크"
     case suhyuppesmb = "헤이뱅크"
     case suhyuppsmb = "파트너뱅크"
-     
+    case postpay = "우체국"
     case jbsmartbank = "전북은행"
     case jbanksmartBankSign = "제주모바일뱅킹"
     case ukbanksmartbank = "케이뱅크"
     case cuonbank = "신협ON뱅크"
+    
+    // 기업
+    case kbbiz = "KB스타기업뱅킹"
+    case hanancbs = "하나은행 기업"
+    case kakaob0a5afe92aad41a59f22497a2b1f6be3 = "NH기업뱅킹"
+    case wooriwonbiz = "우리WON기업"
+    case SmartBankWBC = "원터치 기업"
+    case iphoneSbizbank = "쏠 비즈"
+    case mcbs = "i-ONE뱅크 기업"
+    case dgbbbanking = "DGB기업뱅킹"
+    case bizsmartkdb = "스마트KDB기업"
+    case suhyupbsmb = "수협뱅크기업"
+    case kakaoae784afda3057dafbde4bb9da80bfdfc = "광주기업뱅킹"
+    case smartsbe = "부산기업뱅킹"
+    case ukbankcbzbank = "케이뱅크 기업"
 }
